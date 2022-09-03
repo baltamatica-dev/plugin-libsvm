b7dfea3ae973a7891c87a7ce46cc92ff0f76669d
Chengyu HAN <cyhan@mail.buct.edu.cn>
2022-09-03 10:57:43 +0800

    svmread: 输入参数检查

--- baltam/libsvm/svmread.m
+++ baltam/libsvm/svmread.m
@@ -5,7 +5,25 @@ function [label_vector, instance_matrix] = svmread(filename)
 %   [label_vector, instance_matrix] = svmread('filename.svm');
 %
 
-[label_vector, _instance_matrix] = __svmread_impl(char(filename));
+%% 输入参数检查
+% -- 参数个数
+if 1 ~= nargin
+    disp("输入参数应为 1 个.");
+    % 引发错误输出帮助
+    __svmread_impl();
+    return;
+end
+
+%% 输入预处理
+% 文件名兼容字符串
+filename_chars = char(filename);
+
+%% 调用 impl 函数
+[label_vector, _instance_matrix] = __svmread_impl(filename_chars);
+
+%% 输出后处理
+% TODO:
+%   - transpose() 支持稀疏数组时
 _todo_instance_matrix_full = full(_instance_matrix);
 instance_matrix = sparse(transpose(_todo_instance_matrix_full));
 
